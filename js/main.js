932014b5fdd71e91aabfe5af99c6ff0f34ebc35d
Goodness <thisismegoodness@gmail.com>
2025-11-22 08:31:33 +0100

    Js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+   document.getElementById("myBtn").addEventListener("click", function px_to_em() {
+      const px = 16;
+      const b = document.getElementById('txt').value;
+      const em = b / px;
+      document.getElementById('disp').innerText = em + "em";
+      
+  });
